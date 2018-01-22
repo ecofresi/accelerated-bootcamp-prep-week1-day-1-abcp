ba8b588f4c3ea11e50ed88886e532a5177274fcd
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-22 22:06:17 +0000

    automatically backed up by learn

--- dataTypes.js
+++ dataTypes.js
@@ -2,5 +2,5 @@ console.log(5 + 7);
 console.log("5" + 7);
 console.log(7 - 5);
 console.log("7" - 5);
-console.log("5" - 7);
+console.log("5" - 7);https://learn.co/tracks/accelerated-prep/week-2/functions-and-scope/day-1-lecture-notes-exercises?batch_id=519&track_id=29982#
 console.log(5 - 7);
